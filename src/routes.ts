be4cae1d558da696e15e321616db8a485f4f34c4
Elcoss <jp14700@gmail.com>
2020-06-16 21:47:40 -0300

    node_module bug fix

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import express from "express";
 const routes = express.Router();
 
 routes.get("/", (req, res) => {
-  return res.json("Hello");
+  return res.json("Hello aaaaa");
 });
 
 export default routes;
